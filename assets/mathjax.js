fafccba0c2d77372ce4d569e5f7711e5376ecc0f
Shawn-McSorley <22706094@student.uwa.edu.au>
2024-08-02 23:52:40 +0800

    hmm

---
+++ assets/mathjax.js
@@ -0,0 +1,7 @@
+// assets/js/mathjax.js or assets/js/katex.js
+document.addEventListener("DOMContentLoaded", function() {
+    var script = document.createElement('script');
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML";
+    document.head.appendChild(script);
+  });
+  
